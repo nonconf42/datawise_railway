8f20b06ba834dea4bbea1f45f32e179945ffda82
nonconf42 <nonconf42@gmail.com>
2025-06-27 01:01:17 +0500

    key deleted

--- backend/llms/llm.py
+++ backend/llms/llm.py
@@ -1,97 +1,3 @@
-# from openai import OpenAI
-# import os
-
-# class LLM:
-#     def __init__(self, platform):
-#         self.platform = platform
-#         if platform == "openai":
-#             api_key = os.environ.get('OPENAI_API_KEY')
-#             self.llm_model = OpenAI(api_key=api_key)
-#         elif platform == 'deepinfra':
-#             api_key = os.environ.get('DEEPINFRA_API_KEY')
-#             self.llm_model = OpenAI(
-#                 base_url="https://api.deepinfra.com/v1/openai",
-#                 api_key=api_key
-#             )
-     
-
-#     def llm_call(self, prompt, model_name="gpt-4o"):
-#         if self.platform == "openai":
-#             output_text = self.llm_model.chat.completions.create(
-#                 model=model_name,
-#                 messages=[
-#                     {"role": "system", "content": "You are a helpful assistant."},
-#                     {"role": "user", "content": prompt}
-#                 ]
-#             )
-                
-#             output_text = output_text.choices[0].message.content
-        
-#         elif self.platform == 'deepinfra':
-#             output_text = self.llm_model.chat.completions.create(
-#                 model=model_name,
-#                 messages=[{"role": "user", "content": prompt}],
-#             )
-#             output_text = output_text.choices[0].message.content
-
-
-        
-#         return output_text
-
-
-
-import os
-from openai import OpenAI
-from anthropic import Anthropic
-
-# class LLM:
-#     def __init__(self, platform):
-#         self.platform = platform
-#         if platform == "openai":
-#             api_key = os.environ.get('OPENAI_API_KEY')
-#             self.llm_model = OpenAI(api_key=api_key)
-#         elif platform == 'deepinfra':
-#             api_key = os.environ.get('DEEPINFRA_API_KEY')
-#             self.llm_model = OpenAI(
-#                 base_url="https://api.deepinfra.com/v1/openai",
-#                 api_key=api_key
-#             )
-#         elif platform == 'anthropic':
-#             api_key = os.environ.get('ANTHROPIC_API_KEY')
-#             self.llm_model = Anthropic(api_key=api_key)
-#         else:
-#             raise ValueError(f"Unsupported platform: {platform}")
-
-#     def llm_call(self, prompt, model_name="gpt-4o"):
-#         if self.platform == "openai":
-#             output_text = self.llm_model.chat.completions.create(
-#                 model=model_name,
-#                 messages=[
-#                     {"role": "system", "content": "You are a helpful assistant."},
-#                     {"role": "user", "content": prompt}
-#                 ]
-#             )
-#             output_text = output_text.choices[0].message.content
-
-#         elif self.platform == 'deepinfra':
-#             output_text = self.llm_model.chat.completions.create(
-#                 model=model_name,
-#                 messages=[{"role": "user", "content": prompt}],
-#             )
-#             output_text = output_text.choices[0].message.content
-
-#         elif self.platform == 'anthropic':
-#             output_text = self.llm_model.messages.create(
-#                 model=model_name,
-#                 messages=[{
-#                     "role": "user",
-#                     "content": prompt
-#                 }],
-#                 max_tokens=20000
-#             )
-#             output_text = output_text.content[0].text
-
-#         return output_text
 
 
 import os
@@ -118,7 +24,6 @@ class LLM:
                 api_key=api_key
             )
         elif platform == 'anthropic':
-            os.environ['ANTHROPIC_API_KEY'] = "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"
             api_key = os.environ.get('ANTHROPIC_API_KEY')
             self.llm_model = Anthropic(api_key=api_key)
         else:
